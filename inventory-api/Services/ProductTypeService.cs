ec8462a472f045677f4ec5c196871380e32cac91
Kenan Amundsen Elkoca <kenan@elkoca.com>
2022-06-18 23:06:04 +0200

    added logic for ProductType, and Vendor Service, that hopefully works

--- inventory-api/Services/ProductTypeService.cs
+++ inventory-api/Services/ProductTypeService.cs
@@ -1,42 +1,103 @@
-﻿using inventory_api.Dto;
+﻿using AutoMapper;
+using inventory_api.Data;
+using inventory_api.Data.Entities;
+using inventory_api.Dto;
+using inventory_api.Extensions;
 using inventory_api.Interfaces;
+using inventory_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace inventory_api.Services;
 
 public class ProductTypeService : IProductTypeService
 {
-    public Task<GetProductTypeResponseDto> CreateAsync(PostProductTypeBodyDto product, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+    private readonly InventoryDbContext _dbContext;
+    public readonly IMapper _mapper;
 
-    public Task<GetProductTypeResponseDto> CreateWithIdAsync(Guid id, PostProductTypeBodyDto product, CancellationToken cancellationToken)
+    public ProductTypeService(InventoryDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _dbContext = context;
+        _mapper = mapper;
     }
-
-    public Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<GetProductTypeListResponseDto> GetByPageAsync(int limit, int page, string? sortBy, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        string defaultSortName = "Created";
+        string sortName;
+        PropertyInfo sortProp;
+        bool sortDesc;
+
+        if (string.IsNullOrWhiteSpace(sortBy))
+        {
+            sortName = defaultSortName;
+            sortDesc = false;
+        }
+        else
+        {
+            //splitting sortby (Propname.direction)
+            var sortByProps = sortBy.Trim().Split(".");
+            sortName = sortByProps.Count() > 0 && sortByProps.Count() < 3 ? sortByProps[0] : defaultSortName;
+            sortDesc = sortByProps[1] == "desc" ? true : false;
+        }
+        //Her bør jeg egentlig først mappe sortName mot Dto, som igjen har en kobling mot DB Modellen
+
+        //Get Property, If not exist, Default prop, else throw
+        sortProp = typeof(ProductType).GetProperty(sortName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance) ??
+            typeof(ProductType).GetProperty(defaultSortName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance) ??
+            throw new Exception("Default Prop not found");
+
+
+        PagedModel<ProductType> productTypes = await _dbContext.ProductTypes
+            .AsNoTracking()
+            .OrderByString(sortProp, sortDesc)
+            .PaginateAsync(page, limit, cancellationToken);
+
+        return _mapper.Map<GetProductTypeListResponseDto>(productTypes);
     }
-
-    public Task<bool> ExistAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<GetProductTypeResponseDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var productType = await _dbContext.ProductTypes
+            .SingleOrDefaultAsync(x => x.ProductTypeId == id);
+
+        if (productType == null)
+            return null;
+
+        return _mapper.Map<GetProductTypeResponseDto>(productType);
     }
-
-    public Task<GetProductTypeResponseDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    public async Task<GetProductTypeResponseDto> CreateAsync(PostProductTypeBodyDto productType, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var newProductType = _mapper.Map<ProductType>(productType);
+        _dbContext.ProductTypes.Add(newProductType);
+        await _dbContext.SaveChangesAsync();
+
+        return _mapper.Map<GetProductTypeResponseDto>(newProductType);
     }
-
-    public Task<GetProductTypeListResponseDto> GetByPageAsync(int limit, int page, string? sortBy, CancellationToken cancellationToken)
+    public async Task<GetProductTypeResponseDto> CreateWithIdAsync(Guid id, PostProductTypeBodyDto productType, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var newProductType = _mapper.Map<ProductType>(productType);
+        newProductType.ProductTypeId = id;
+
+        _dbContext.ProductTypes.Add(newProductType);
+        await _dbContext.SaveChangesAsync();
+
+        return _mapper.Map<GetProductTypeResponseDto>(newProductType);
     }
-
-    public Task ReplaceAsync(PutProductTypeBodyDto product, CancellationToken cancellationToken)
+    public async Task ReplaceAsync(PutProductTypeBodyDto productType, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var replacedProductType = _mapper.Map<ProductType>(productType);
+        _dbContext.ProductTypes.Attach(replacedProductType);
+        _dbContext.ProductTypes.Update(replacedProductType);
+        await _dbContext.SaveChangesAsync();
+    }
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var productType = new ProductType { ProductTypeId = id };
+        _dbContext.ProductTypes.Attach(productType);
+        _dbContext.ProductTypes.Remove(productType);
+        await _dbContext.SaveChangesAsync();
+    }
+    public async Task<bool> ExistAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbContext.ProductTypes.AnyAsync(e => e.ProductTypeId == id);
     }
 }
--- inventory-api/Services/VendorService.cs
+++ inventory-api/Services/VendorService.cs
@@ -1,42 +1,104 @@
-﻿using inventory_api.Dto;
+﻿using AutoMapper;
+using inventory_api.Data;
+using inventory_api.Data.Entities;
+using inventory_api.Dto;
+using inventory_api.Extensions;
 using inventory_api.Interfaces;
+using inventory_api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace inventory_api.Services;
 
 public class VendorService : IVendorService
 {
-    public Task<GetVendorResponseDto> CreateAsync(PostVendorBodyDto product, CancellationToken cancellationToken)
+    private readonly InventoryDbContext _dbContext;
+    public readonly IMapper _mapper;
+
+    public VendorService(InventoryDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _dbContext = context;
+        _mapper = mapper;
     }
 
-    public Task<GetVendorResponseDto> CreateWithIdAsync(Guid id, PostVendorBodyDto product, CancellationToken cancellationToken)
+    public async Task<GetVendorListResponseDto> GetByPageAsync(int limit, int page, string? sortBy, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
-    }
+        string defaultSortName = "Created";
+        string sortName;
+        PropertyInfo sortProp;
+        bool sortDesc;
 
-    public Task DeleteAsync(Guid id, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+        if (string.IsNullOrWhiteSpace(sortBy))
+        {
+            sortName = defaultSortName;
+            sortDesc = false;
+        }
+        else
+        {
+            //splitting sortby (Propname.direction)
+            var sortByProps = sortBy.Trim().Split(".");
+            sortName = sortByProps.Count() > 0 && sortByProps.Count() < 3 ? sortByProps[0] : defaultSortName;
+            sortDesc = sortByProps[1] == "desc" ? true : false;
+        }
+        //Her bør jeg egentlig først mappe sortName mot Dto, som igjen har en kobling mot DB Modellen
 
-    public Task<bool> ExistAsync(Guid id, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+        //Get Property, If not exist, Default prop, else throw
+        sortProp = typeof(Vendor).GetProperty(sortName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance) ??
+            typeof(Vendor).GetProperty(defaultSortName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance) ??
+            throw new Exception("Default Prop not found");
 
-    public Task<GetVendorResponseDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
 
-    public Task<GetVendorListResponseDto> GetByPageAsync(int limit, int page, string? sortBy, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+        PagedModel<Vendor> vendors = await _dbContext.Vendors
+            .AsNoTracking()
+            .OrderByString(sortProp, sortDesc)
+            .PaginateAsync(page, limit, cancellationToken);
 
-    public Task ReplaceAsync(PutVendorBodyDto product, CancellationToken cancellationToken)
+        return _mapper.Map<GetVendorListResponseDto>(vendors);
+    }
+    public async Task<GetVendorResponseDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var vendors = await _dbContext.Vendors
+            .SingleOrDefaultAsync(x => x.VendorId == id);
+
+        if (vendors == null)
+            return null;
+
+        return _mapper.Map<GetVendorResponseDto>(vendors);
+    }
+    public async Task<GetVendorResponseDto> CreateAsync(PostVendorBodyDto vendor, CancellationToken cancellationToken)
+    {
+        var newVendor = _mapper.Map<Vendor>(vendor);
+        _dbContext.Vendors.Add(newVendor);
+        await _dbContext.SaveChangesAsync();
+
+        return _mapper.Map<GetVendorResponseDto>(newVendor);
+    }
+    public async Task<GetVendorResponseDto> CreateWithIdAsync(Guid id, PostVendorBodyDto vendor, CancellationToken cancellationToken)
+    {
+        var newVendor = _mapper.Map<Vendor>(vendor);
+        newVendor.VendorId = id;
+
+        _dbContext.Vendors.Add(newVendor);
+        await _dbContext.SaveChangesAsync();
+
+        return _mapper.Map<GetVendorResponseDto>(newVendor);
+    }
+    public async Task ReplaceAsync(PutVendorBodyDto vendor, CancellationToken cancellationToken)
+    {
+        var replacedVendor = _mapper.Map<Vendor>(vendor);
+        _dbContext.Vendors.Attach(replacedVendor);
+        _dbContext.Vendors.Update(replacedVendor);
+        await _dbContext.SaveChangesAsync();
+    }
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var vendor = new Vendor { VendorId = id };
+        _dbContext.Vendors.Attach(vendor);
+        _dbContext.Vendors.Remove(vendor);
+        await _dbContext.SaveChangesAsync();
+    }
+    public async Task<bool> ExistAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _dbContext.Vendors.AnyAsync(e => e.VendorId == id);
     }
 }
